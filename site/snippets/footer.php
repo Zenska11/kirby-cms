e5ebfd59319a3b978a30aff031dd3aec7e60ff78
Zenska11 <zenska96@gmx.de>
2022-04-07 12:25:13 +0200

    templates and snippets

---
+++ site/snippets/footer.php
@@ -0,0 +1,6 @@
+<footer>
+    <a href="https://twitter.com/<?= $site->twitter() ?>">Follow us on Twitter</a>
+</footer>
+</body>
+
+</html>
--- site/templates/default.php
+++ site/templates/default.php
@@ -1,1 +1,8 @@
-<h1><?= $page->title() ?></h1>
+<?php snippet('header') ?>
+
+<main class="main">
+    <h1><?= $page->title() ?></h1>
+    <?= $page->text() ?>
+</main>
+
+<?php snippet('footer') ?>

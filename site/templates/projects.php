7e290357714ff0f28581d4a933c3147d8d3872b4
Zenska11 <zenska96@gmx.de>
2022-04-08 09:45:21 +0200

    pagination

--- site/templates/projects.php
+++ site/templates/projects.php
@@ -1,11 +1,15 @@
 <?php snippet('header') ?>
+<?php 
+$projects = $page->children()->listed()->paginate(3);
+$pagination = $projects->pagination();
+?>
 
 <main class="main">
     <h1><?= $page->title() ?></h1>
 
 
     <ul class="projects">
-        <?php foreach ($page->children()->listed() as $project) : ?>
+        <?php foreach ($projects as $project) : ?>
             <li>
                 <a href="<?= $project->url() ?>">
                     <figure>
@@ -19,6 +23,24 @@
         <?php endforeach ?>
     </ul>
 
+
+    <?php if ($pagination->hasPages()): ?>
+    <nav class="pagination">
+        <?php if ($pagination->hasPrevPage()): ?>
+        <a href="<?= $pagination->prevPageUrl() ?>" arial-label="Go to previous page">&larr;</a>
+        <?php else: ?>
+        <span>&larr;</span>
+        <?php endif ?>
+
+        <span>Page <?= $pagination->page() ?>  of <?= $pagination->pages() ?> </span>
+
+        <?php if ($pagination->hasNextPage()): ?>
+        <a href="<?= $pagination->nextPageUrl() ?>" arial-label="Go to next page">&rarr;</a>
+        <?php else: ?>
+        <span>&rarr;</span>
+        <?php endif ?>
+    </nav>
+    <?php endif ?>
 
 </main>
 
